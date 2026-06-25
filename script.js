a158222c985e3fa25e3b7a237bad9d8cdc75b1ab
Sayan Adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-06-25 09:20:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species = species;
+	}
+	get(){
+		return this.species;
+	}
+	makeSound(){
+		return `The ${this.species} makes a sound`;
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	constructor(abc){
+		super(abc);
+	}
+	bark(){
+		return `woof`;
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	constructor(abc){
+		super(abc);
+	}
+	purr(){
+		return `purr`;
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
